e0dacf92b4895c1725913e43c41b7b42d04f6c0f
Denis Manente <denis.manente@automatic-server.com>
2020-07-22 12:50:58 +0200

    example imports

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/dionysius/issue-lxd-7677-consumer
 
 go 1.14
 
-require github.com/dionysius/issue-lxd-7677-lib v0.0.0-20200722104248-a83a167b5fa7
+require github.com/dionysius/issue-lxd-7677-lib v0.0.0-20200722112053-5c8d66bc8753
--- main.go
+++ main.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 
 	lxd "github.com/dionysius/issue-lxd-7677-lib/lxd"
+	// uncomment to see what each of these do in the vendor/ folder
+	// _ "github.com/dionysius/issue-lxd-7677-lib/lxd/include1"
+	// _ "github.com/dionysius/issue-lxd-7677-lib/lxd/include2"
+	// _ "github.com/dionysius/issue-lxd-7677-lib/lxd/include3"
+	_ "github.com/dionysius/issue-lxd-7677-lib/lxd/implementation"
 )
 
 func main() {
