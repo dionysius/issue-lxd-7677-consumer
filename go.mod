3946cd8f5db25d13af59c30b0ffccb55dac2e6a4
Denis Manente <denis.manente@automatic-server.com>
2020-07-22 12:45:02 +0200

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dionysius/issue-lxd-7677-consumer
+
+go 1.14
+
+require github.com/dionysius/issue-lxd-7677-lib v0.0.0-20200722104248-a83a167b5fa7
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	lxd "github.com/dionysius/issue-lxd-7677-lib/lxd"
+)
+
+func main() {
+	fmt.Printf("Hello %s\n", lxd.Something)
+}
